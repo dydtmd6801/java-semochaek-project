9e7add929584cd48b13069cdc166b264472951ca
dydtmd6801 <dydtmd6801@naver.com>
2025-06-16 19:52:29 +0900

    fix: Modify import Book
    Book을 import하는 과정에서 다른 Book을 넣어서 에러 발생
    Entity에 있는 Book을 import함으로 에러 해결

--- src/main/java/com/project/repository/BookRepository.java
+++ src/main/java/com/project/repository/BookRepository.java
@@ -1,9 +1,10 @@
 package com.project.repository;
 
-import java.awt.print.Book;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.entity.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 	Optional<Book> findByIsbn(String isbn);
