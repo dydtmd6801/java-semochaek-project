ea55e7d296b4e2a0388a7b8d6f70bf7f28289f27
dydtmd6801 <dydtmd6801@naver.com>
2025-06-16 18:57:47 +0900

    feat: Add Books Entity

---
+++ src/main/java/com/project/Entity/Book.java
@@ -0,0 +1,50 @@
+package com.project.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "books")
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String title;
+	
+	@Column(nullable = false)
+	private String author;
+	
+	@Column(nullable = false)
+	private String publisher;
+	
+	@Column(nullable = false, unique = true)
+	private String isbn;
+	
+	@Column(nullable = false)
+	private String image;
+	
+	@Column(nullable = false, length = 1000)
+	private String description;
+	
+	@Column(nullable = false)
+	private int price;
+	
+	private String genreLv1;
+	private String genreLv2;
+	private String genreLv3;
+}
