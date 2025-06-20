762b50268e657b367a72482a9d5519547858d537
dydtmd6801 <dydtmd6801@naver.com>
2025-06-20 13:36:29 +0900

    feat: Add User
    JPA 리포지토리 사용을 위해 User Entity 생성

---
+++ src/main/java/com/project/Entity/User.java
@@ -0,0 +1,50 @@
+package com.project.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Builder
+@Table(name = "users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private long userId;
+	
+	@Column(nullable = false, unique = true)
+	private String email;
+	
+	@Column(nullable = false)
+	private String password;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false)
+	private String telephone;
+	
+	@Column(nullable = false)
+	private String role;
+	
+	@Column(nullable = false, length = 1)
+	private String status;
+	
+	@Column(nullable = true)
+	private String address;
+	
+	@Column(nullable = true)
+	private String detailAddress;
+	
+	@Column(nullable = true)
+	private int postNumber;
+	
+}
