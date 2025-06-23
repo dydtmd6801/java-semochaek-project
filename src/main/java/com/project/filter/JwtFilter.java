75a0eedc92ba01e237d5d0201720c4e8d7bdcb32
dydtmd6801 <dydtmd6801@naver.com>
2025-06-23 12:48:31 +0900

    feat: Add JwtFilter
    JWT 토큰을 요청마다 검사하기 위한 커스텀 필터 클래스

---
+++ src/main/java/com/project/filter/JwtFilter.java
@@ -0,0 +1,51 @@
+package com.project.filter;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.project.service.CustomUserDetailService;
+import com.project.util.JwtUtil;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+	
+	private final JwtUtil jwtUtil;
+	private final CustomUserDetailService customUserDetailService;
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, 
+			HttpServletResponse response, 
+			FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		String authHeader = request.getHeader("Authorization");
+		
+		if (authHeader != null && authHeader.startsWith("Bearer")) {
+			String token = authHeader.substring(7);
+			
+			if (jwtUtil.validationToken(token)) {
+				String username = jwtUtil.extrasctUsername(token);
+				UserDetails userDetails = customUserDetailService.loadUserByUsername(username);
+				
+				UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+				SecurityContextHolder.getContext().setAuthentication(authToken);
+			}
+		}
+		
+		filterChain.doFilter(request, response);
+	}
+	
+	
+}
