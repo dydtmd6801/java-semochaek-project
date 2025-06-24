dee0c342532252c003aceeb8ec3e75d8d7698d00
dydtmd6801 <dydtmd6801@naver.com>
2025-06-24 14:57:16 +0900

    fix: Modify Config
    "/"가 빠져서 추가

--- src/main/java/com/project/config/SecurityConfig.java
+++ src/main/java/com/project/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
 		return http
 				.csrf(csrf -> csrf.disable())
 				.authorizeHttpRequests(auth -> auth
-					.requestMatchers("/login", "register").permitAll()
+					.requestMatchers("/login", "/register").permitAll()
 					.anyRequest().authenticated()
 				)
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
