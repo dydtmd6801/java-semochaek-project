cfe5989a333f2f76683dca9528ca7bbe0482a5fe
dydtmd6801 <dydtmd6801@naver.com>
2025-06-26 15:51:49 +0900

    feat: Add MailController
    사용자의 이메일을 받아 인증메일을 전송하고, 인증번호를 받아 검증까지 하는 클래스

---
+++ src/main/java/com/project/controller/MailController.java
@@ -0,0 +1,42 @@
+package com.project.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.data.VerifyRequest;
+import com.project.service.MailService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class MailController {
+	
+	private final MailService mailService;
+	private final Map<String, String> verificationCodes = new HashMap<>();
+	
+	@PostMapping("/send-code")
+	public ResponseEntity<?> sendCode(@RequestBody String email) {
+		String code = UUID.randomUUID().toString().substring(0, 6);
+		mailService.sendVerificationEmail(email, code);
+		verificationCodes.put(email, code);
+		return ResponseEntity.ok("인증코드가 전송되었습니다.");
+	}
+	
+	@PostMapping("/verify-code")
+	public ResponseEntity<?> verifyCode(@RequestBody VerifyRequest request) {
+		String savedCode = verificationCodes.get(request.getEmail());
+		System.out.println("saved : " + savedCode + " , code : " + request.getCode());
+		if (savedCode != null && savedCode.equals(request.getCode())) {
+			return ResponseEntity.ok("이메일 인증 완료");
+		} else {
+			return ResponseEntity.badRequest().body("코드가 일치하지 않습니다.");
+		}
+	}
+}
