ad03e2d959fd991d13841dcadfb071240229726e
dydtmd6801 <dydtmd6801@naver.com>
2025-06-26 22:09:52 +0900

    feat: Add resetPassword (UserConrtroller.java)
    사용자에게 받은 Email과 Password값으로 비밀번호 업데이트 기능을 실행 시키는 기능

--- src/main/java/com/project/controller/UserController.java
+++ src/main/java/com/project/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.data.ApproveRequest;
+import com.project.data.AuthRequest;
 import com.project.data.FindEmailRequest;
 import com.project.data.RegisterRequest;
 import com.project.service.UserService;
@@ -40,6 +41,16 @@ public class UserController {
 		try {
 			String email = userService.loadEmail(request.getName(), request.getPhoneNumber());
 			return ResponseEntity.ok(email);
+		} catch (EntityNotFoundException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+	
+	@PostMapping("/resetPassword")
+	public ResponseEntity<?> resetPassword (@RequestBody AuthRequest request) {
+		try {
+			userService.updatePassword(request.getEmail(), request.getPassword());
+			return ResponseEntity.ok("비밀번호 수정 완료");
 		} catch (EntityNotFoundException e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
