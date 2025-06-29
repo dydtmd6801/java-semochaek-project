a92fd2cbd295a4d4750f6d2703a11a72ea8ca5f6
dydtmd6801 <dydtmd6801@naver.com>
2025-06-29 22:24:36 +0900

    fix: Add Anotation (Cart.java)
    Builder 애노테이션 사용을 위해 기본 생성자를 만들어주는 애노테이션 추가

--- src/main/java/com/project/Entity/Cart.java
+++ src/main/java/com/project/Entity/Cart.java
@@ -9,9 +9,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Getter
 @Entity
