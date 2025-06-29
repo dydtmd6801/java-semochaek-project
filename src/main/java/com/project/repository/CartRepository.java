6ea787e29d2628ac87f66879a5cb4d580178e439
dydtmd6801 <dydtmd6801@naver.com>
2025-06-29 23:19:57 +0900

    feat: Add deleteByUserAndBook (CartRepository.java)
    User와 Book을 기준으로 삭제하는 쿼리 추가

--- src/main/java/com/project/repository/CartRepository.java
+++ src/main/java/com/project/repository/CartRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.project.entity.Book;
 import com.project.entity.Cart;
 import com.project.entity.User;
 
 public interface CartRepository extends JpaRepository<Cart, Long>{
 	List<Cart> findAllByUser(User user);
+	void deleteByUserAndBook(User user, Book book);
 }
