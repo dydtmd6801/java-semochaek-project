1eb1b7e36283aa144698056c57aeee3672434d3a
dydtmd6801 <dydtmd6801@naver.com>
2025-06-30 22:13:38 +0900

    feat: Add findAllBy~ (OrderRepository.java, DetailOrderRepository.java)
    JPA에 있는 findAllBy~ 메소드를 통해 해당 조건에 만족하는 모든 데이터를 List로 반환

--- src/main/java/com/project/repository/DetailOrderRepository.java
+++ src/main/java/com/project/repository/DetailOrderRepository.java
@@ -1,9 +1,12 @@
 package com.project.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.entity.DetailOrder;
+import com.project.entity.Order;
 
 public interface DetailOrderRepository extends JpaRepository<DetailOrder, Long> {
-
+	List<DetailOrder> findByOrder(Order order);
 }
--- src/main/java/com/project/repository/OrderRepository.java
+++ src/main/java/com/project/repository/OrderRepository.java
@@ -1,9 +1,12 @@
 package com.project.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.entity.Order;
+import com.project.entity.User;
 
-public interface OrderRepository extends JpaRepository<Order, String> {
-
+public interface OrderRepository extends JpaRepository<Order, Long> {
+	List<Order> findAllByUser(User user);
 }
