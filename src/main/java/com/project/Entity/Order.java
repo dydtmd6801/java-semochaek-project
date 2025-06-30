f585531fd88a54ecfd2230bbc051d2d27cdf88c3
dydtmd6801 <dydtmd6801@naver.com>
2025-06-30 21:55:06 +0900

    feat: Add Column (Order.java)
    사용자 구분을 위한 email 칼럼 추가

--- src/main/java/com/project/Entity/Order.java
+++ src/main/java/com/project/Entity/Order.java
@@ -4,9 +4,12 @@ import java.util.Date;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
@@ -37,4 +40,8 @@ public class Order {
 	
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date paidAt;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
 }
