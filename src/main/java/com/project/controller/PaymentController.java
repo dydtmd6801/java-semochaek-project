3f3281269b32b3b4052b3fe6d4ae77f20eec3f51
dydtmd6801 <dydtmd6801@naver.com>
2025-06-30 23:11:08 +0900

    feat: Add searchPayment (PaymentController.java)
    주문 내역을 불러오는 기능에 email을 넘겨주고 결과를 List로 받아서 프론트로 반환하는 기능

--- src/main/java/com/project/controller/PaymentController.java
+++ src/main/java/com/project/controller/PaymentController.java
@@ -1,11 +1,16 @@
 package com.project.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.data.OrderResponse;
 import com.project.data.PaymentVerifyRequest;
 import com.project.service.PaymentService;
 
@@ -22,5 +27,11 @@ public class PaymentController {
 	public ResponseEntity<String> verify(@RequestBody PaymentVerifyRequest request) {
 		paymentService.verifyAndSave(request);
 		return ResponseEntity.ok("결제 검증 및 저장 완료");
+	}
+	
+	@GetMapping("/search")
+	public ResponseEntity<List<OrderResponse>> searchPayment() {
+		String email = SecurityContextHolder.getContext().getAuthentication().getName();
+		return ResponseEntity.ok(paymentService.getOrderList(email));
 	}
 }
