6ac1d7901a74b25d92e82cbeb4920441e0a83170
dydtmd6801 <dydtmd6801@naver.com>
2025-07-01 23:37:11 +0900

    feat: Add findAll(pageable) (BookRepository.java)
    도서 목록 전체를 받아오는 기능 중 페이지 기능을 추가하여 받아오는 기능 추가

--- src/main/java/com/project/repository/BookRepository.java
+++ src/main/java/com/project/repository/BookRepository.java
@@ -2,10 +2,13 @@ package com.project.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.entity.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 	Optional<Book> findByIsbn(String isbn);
+	Page<Book> findAll(Pageable pageable);
 }
