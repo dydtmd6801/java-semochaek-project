7d2546a1e9cb354caf9027b540e047794d61f8b0
dydtmd6801 <dydtmd6801@naver.com>
2025-07-03 17:15:11 +0900

    feat: Add errorHandling (login.js)
    로그인 시, 에러가 발생할 경우 사용자에게 에러를 알려주고, email 입력칸을 클릭하면 에러문구가 사라지는 기능 추가

--- src/main/resources/static/javaScript/login.js
+++ src/main/resources/static/javaScript/login.js
@@ -1,6 +1,8 @@
 const btnLogin = document.getElementById("btn-login");
 const userEmail = document.getElementById("email");
 const userPassword = document.getElementById("password");
+const errorWrapper = document.getElementById("error-wrapper");
+
 btnLogin.addEventListener("click", () => {
     const request = {
         email : userEmail.value,
@@ -10,5 +12,18 @@ btnLogin.addEventListener("click", () => {
         .then(response => {
             const token = response.data.token;
             localStorage.setItem("token", token);
+            location.href = "index.html";
         })
+        .catch(() => {
+            if (!document.getElementsByClassName("error")[0]) {
+                const loginError = document.createElement("p");
+                loginError.className = "error"
+                loginError.textContent = "⚠️이메일 혹은 비밀번호가 올바르지 않습니다.";
+                errorWrapper.appendChild(loginError);
+            }
+        })
+})
+
+userEmail.addEventListener("focusin", () => {
+    document.getElementsByClassName("error")[0].remove();
 })
