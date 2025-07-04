1024010daf6d56d6e081812bde881b740e01718a
dydtmd6801 <dydtmd6801@naver.com>
2025-07-05 01:50:48 +0900

    fix: Modify request And Add CrossOrigin (CartController.java)
    get 방식으로 통신하기 때문에 json보다는 queryString으로 isbn을 받을 수 있도록 수정
    CORS 에러를 해결하기 위해 CrossOrigin 어노테이션 추가

--- src/main/java/com/project/controller/CartController.java
+++ src/main/java/com/project/controller/CartController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.data.CartRequest;
@@ -16,6 +18,7 @@ import com.project.service.CartService;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RestController
 @RequiredArgsConstructor
 public class CartController {
@@ -23,10 +26,10 @@ public class CartController {
 	private final CartService cartService;
 	
 	@GetMapping("/addCart")
-	public ResponseEntity<?> addCart(@RequestBody CartRequest request) {
+	public ResponseEntity<?> addCart(@RequestParam("isbn") String isbn) {
 		try {
 			String email = SecurityContextHolder.getContext().getAuthentication().getName();
-			cartService.saveCart(email, request);
+			cartService.saveCart(email, isbn);
 			return ResponseEntity.ok("저장 완료");
 		} catch (EntityNotFoundException e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
