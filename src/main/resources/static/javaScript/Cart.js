1aa2aa5e1d522854dd96db5d603eb6bd4a582780
dydtmd6801 <dydtmd6801@naver.com>
2025-07-05 16:00:03 +0900

    fix: Add location.href And Modify Error Handling
    로그인이 안되어있거나 만료된 토큰을 가지고 있다면, 로그인 화면으로 바로가는 기능 추가
    axios를 통해 api를 호출하고 에러가 났을 경우, 핸들링 수정

--- src/main/resources/static/javaScript/Cart.js
+++ src/main/resources/static/javaScript/Cart.js
@@ -5,11 +5,13 @@ function addCart(isbn) {
 
     if (token == null) {
         alert("로그인이 필요합니다.");
+        location.href="login.html";
         return;
     }
 
     if (validator.validateToken(token)) {
         alert("로그인이 만료되었습니다. 다시 로그인 해주세요.");
+        location.href="login.html";
         return;
     }
 
@@ -20,7 +22,7 @@ function addCart(isbn) {
         }
     })
     .then(() => alert("장바구니에 추가되었습니다!"))
-    .erro(() => alert("에러가 발생하였습니다. 관리자에게 문의 부탁드립니다."));
+    .catch(() => alert("에러가 발생하였습니다. 관리자에게 문의 부탁드립니다."));
 }
 
 export default {addCart};
