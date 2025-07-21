998e919cb5f3249606c57dd445759648f6ededba
dydtmd6801 <dydtmd6801@naver.com>
2025-07-21 18:44:47 +0900

    feat: Add getBooksSlice(BookService.java)
    전체 책 데이터를 Slice<T> 타입으로 반환하는 기능

--- src/main/java/com/project/service/BookService.java
+++ src/main/java/com/project/service/BookService.java
@@ -1,15 +1,19 @@
 package com.project.service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
-
+import com.project.controller.AuthController;
 import com.project.data.BookDTO;
 import com.project.data.BookDetailDTO;
 import com.project.entity.Book;
 import com.project.repository.BookRepository;
+import com.project.repository.BookRepositorySlice;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -19,11 +23,25 @@ import lombok.RequiredArgsConstructor;
 public class BookService {
 	
 	private final BookRepository bookRepository;
+	private final BookRepositorySlice bookRepositorySlice;
 	
-	public Page<BookDTO> getAllBooks(Pageable pageable) {
+	public Page<BookDTO> getAllBooksPage(Pageable pageable) {
 		Page<Book> books = bookRepository.findAllByPriceNot(0, pageable);
 		
 		return books.map((Book book) -> BookDTO.builder()
+						.title(book.getTitle())
+						.author(book.getAuthor())
+						.publisher(book.getPublisher())
+						.isbn(book.getIsbn())
+						.image(book.getImage())
+						.price(book.getPrice())
+						.build());
+	}
+	
+	public Slice<BookDTO> getAllBooksSlice(Pageable pageable) {
+		Slice<Book> books = bookRepositorySlice.findAllByPriceNot(0, pageable);
+		
+		return books.map(book -> BookDTO.builder()
 						.title(book.getTitle())
 						.author(book.getAuthor())
 						.publisher(book.getPublisher())
