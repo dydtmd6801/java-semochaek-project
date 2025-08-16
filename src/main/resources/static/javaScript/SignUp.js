67c595b9b26335153bdb9765e6a2821048fb9ddd
dydtmd6801 <dydtmd6801@naver.com>
2025-08-16 22:19:55 +0900

    feat: Add send email (SignUp.js)
    사용자가 입력한 이메일을 받아서 이메일 전송 API를 호출한 뒤
    올바른 이메일이면, 이메일 전송 완료 알림을 보내고, 인증코드 입력칸 생성
    올바르지 않은 이메일이면, 에러 알림을 보내고 다시 입력 받기

--- src/main/resources/static/javaScript/SignUp.js
+++ src/main/resources/static/javaScript/SignUp.js
@@ -1,6 +1,9 @@
 const password = document.getElementById("passwordValue");
 const confirmPassword = document.getElementById("confirmPassword");
 const passwordDiv = document.getElementById("password");
+const email = document.getElementById("emailValue");
+const sendMailBtn = document.getElementById("send-mail-btn");
+const emailDiv = document.getElementById("email");
 
 let passwordChecker = false;
 
@@ -15,6 +18,24 @@ function checkPassword() {
     }
 }
 
+function sendMail() {
+    axios.post("http://localhost:8080/send-code", 
+        String(email.value), 
+        {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+        )
+    .then(() => {
+        alert("인증번호가 전송되었습니다.");
+    })
+    .catch(() => {
+        alert("올바른 이메일을 입력해주세요");
+        location.reload(true);
+    })
+}
+
 password.addEventListener("focusin", () => {
     document.getElementsByClassName("error")[0].remove();
 })
@@ -22,3 +43,21 @@ confirmPassword.addEventListener("focusin", () => {
     document.getElementsByClassName("error")[0].remove();
 })
 confirmPassword.addEventListener("focusout",checkPassword);
+
+sendMailBtn.addEventListener("click",() => {
+    sendMail();
+    const emailBtn = document.getElementsByClassName("emailBtn");
+    if (emailBtn.length < 2) {
+        const div = document.createElement("div");
+        div.className = "emailBtn";
+        const codeInput = document.createElement("input");
+        codeInput.type = "input";
+        codeInput.className = "codeInput"
+        const verifyBtn = document.createElement("button");
+        verifyBtn.textContent = "인증받기"
+        div.appendChild(codeInput);
+        div.appendChild(verifyBtn);
+        emailDiv.appendChild(div);
+        sendMailBtn.textContent = "재전송";
+    }
+});
