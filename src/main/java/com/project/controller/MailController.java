823cd886f44aefd8e11ac9058c02e22b30f950fb
dydtmd6801 <dydtmd6801@naver.com>
2025-08-16 22:18:56 +0900

    feat: Add CrossOrigin anotation (MailController.java)
    이메일 전송을 위해 프론트와의 통신 중 발생하는 CORS를 방지하기 위해 CrossOrigin 애노테이션 추가

--- src/main/java/com/project/controller/MailController.java
+++ src/main/java/com/project/controller/MailController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import com.project.service.MailService;
 
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RestController
 @RequiredArgsConstructor
 public class MailController {
