be0bacdcfbc2fc82f7d311de54039ff0fc6b9fa8
dydtmd6801 <dydtmd6801@naver.com>
2025-08-23 12:52:42 +0900

    feat: Add Favorite Entity (Favorite.java)
    JPA를 사용하기 위해 Favorite 테이블과 매칭되는 Entity 추가

---
+++ src/main/java/com/project/Entity/Favorite.java
@@ -0,0 +1,38 @@
+package com.project.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "favorite")
+public class Favorite {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "favorite_id")
+	private Long favoriteId;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "book_id", nullable = false)
+	private Book book;
+
+}
